3129918d1939fee0992a4c3bf6c30af7abaf5226
heping <3397499417@qq.com>
2024-07-05 17:28:15 +0800

    main

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import sys
-
-
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
+from ui import info
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == '__main__':
-    # 创建QApplication对象
-    app = QApplication(sys.argv)
-    # 创建窗口对象
-    w = QMainWindow()
-    # 设置窗口尺寸
-    w.resize(300, 200)
-    # 移动窗口
-    w.move(300, 300)
-    # 设置窗口标题
-    w.setWindowTitle('窗口')
-
-    w.show()
-
-    # 进入主循环
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = info.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
