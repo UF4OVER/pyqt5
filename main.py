a928ed15fec320d12a7e5e2929d5bcfb04763c5c
heping <3397499417@qq.com>
2024-07-12 23:16:25 +0800

    TWO

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
-from ui import info
+from ui import 计算器
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = info.Ui_MainWindow()
+    ui = 计算器.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
